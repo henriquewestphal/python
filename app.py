550d9fb9df414f3568e096b52e7e1efa930f31ac
Henrique Westphal <henriquewalves@hotmail.com>
2017-04-13 00:23:57 -0300

    funções, regex, alterar, procudar

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 # -*- coding: UTF-8 -*-
+import re
+
 def cadastrar(nomes):
     print 'Digiteo nome:'
     nome = raw_input()
@@ -14,6 +16,39 @@ def remover(nomes):
     nome_remove = raw_input()
     nomes.remove(nome_remove)
 
+def alterar(nomes):
+    print 'Qual nome voce deseja alterar?'
+    nome_alterar = raw_input()
+    existe = nome_alterar in nomes
+    if (existe == True):
+        print 'Nome encontrado'
+        posicao = nomes.index(nome_alterar)
+        print 'deseja alterar para qual nome?'
+        novo_nome = raw_input()
+        nomes[posicao] = novo_nome
+        print 'nome alterado com sucesso'
+
+    if (existe == False):
+        print 'nome não encontrado'
+
+
+def procurar(nomes):
+    print 'Digite o nome a procurar'
+    procura_nome = raw_input()
+    existe = procura_nome in nomes
+    if (existe == True):
+        print 'nome encontrado'
+
+    if (existe == False):
+        print 'nome não encontrado'
+
+def procurar_regex(nomes):
+    print('Digite a expressão regular')
+    regex = raw_input()
+    nomes_concatenados = ' '.join(nomes)
+    resultados = re.findall(regex, nomes_concatenados)
+    print(resultados)
+
 
 def menu():
     nomes = []
@@ -23,7 +58,11 @@ def menu():
         print " 1 para cadastrar"
         print " 2 para listar"
         print " 3 para remover"
+        print " 4 para alterar"
+        print " 5 para procurar"
+        print " 6 para procurar por regex"
         print " 0 para terminar"
+
         escolha = raw_input()
 
         if (escolha == '1'):
@@ -34,5 +73,16 @@ def menu():
 
         if (escolha == '3'):
             remover(nomes)
+
+        if (escolha == '4'):
+            alterar(nomes)
+
+        if (escolha == '5'):
+            procurar(nomes)
+
+        if (escolha == '6'):
+            procurar_regex(nomes)
+
+
 
 menu()
